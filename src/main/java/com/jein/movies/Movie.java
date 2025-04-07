d0ce5b55c49b0842503e0519329375ea5b48c2cd
JeinKh <juliakhezhun@gmail.com>
2025-04-07 18:10:24 +0300

    changes

---
+++ src/main/java/com/jein/movies/Movie.java
@@ -0,0 +1,33 @@
+package com.jein.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+@Document(collection = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+    static String genre = getDefaultGenre(); // if getDefaultGenre() throws an exception...
+    static String getDefaultGenre() {
+        throw new RuntimeException("Oops"); // triggers ExceptionInInitializerError
+    }
+    @Id
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+    @DocumentReference
+    private List<Review> reviewIds;
+}
