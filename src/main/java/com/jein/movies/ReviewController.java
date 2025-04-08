a4b5eef6d455a0f9eb3d5db57ac10c6593d614f2
JeinKh <juliakhezhun@gmail.com>
2025-04-08 15:26:03 +0300

    change  routs in controllers

--- src/main/java/com/jein/movies/ReviewController.java
+++ src/main/java/com/jein/movies/ReviewController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping("api/v1/reviews")
+@RequestMapping("api/reviews")
 
 public class ReviewController {
     @Autowired
